ea15abf9035ab30f839c34e32809f94baa6d5ce4
Praveen <154012512+23MH1A0550@users.noreply.github.com>
2023-12-28 17:08:39 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d",&a,&b);
        for(c=1;c<=b;c++)
        {
            d=a*c;
            printf("%d x %d = %d
    ",a,c,d);
            printf("");
        }
    }

---
+++ Multiplication_Table-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d",&a,&b);
+    
+    for(c=1;c<=b;c++)
+    {
+        d=a*c;
+        printf("%d x %d = %d
+",a,c,d);
+        printf("");
+    }
+}
